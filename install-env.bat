c9ed85633697f61b9d2dd4ad3d067cc631feb26a
Yuki MITA <mitayuki3@yahoo.co.jp>
2022-08-01 21:01:34 +0900

    add KeePassXC and MEGAsync

--- install-env.bat
+++ install-env.bat
@@ -2,3 +2,5 @@ winget install --source msstore firefox
 winget install --source winget git
 winget install tortoisegit
 winget install microsoft.visualstudiocode
+winget install KeePassXC
+winget install MEGAsync
